9aa2653d16d357a95b78f2ed95b698672c22aa54
M3galos <juannava67@live.com>
2025-04-08 21:57:59 -0600

    author, post and comments are now added to the admin view

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Post, Comment
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+    search_fields = ('name', 'email')
+    
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at', 'published')
+    list_filter = ('published', 'created_at')
+    search_fields = ('title', 'content')
+    
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('author', 'content')
+    
